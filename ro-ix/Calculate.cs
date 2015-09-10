ff244a414369d74cd3ebca0e62f41fc4df778b33
Sasha_work <sashapvt@gmail.com>
2015-09-10 17:37:08 +0300

    Створено клас для розрахунку проекту

---
+++ ro-ix/Calculate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RO_IX
+{
+    // Клас для розрахунку проекту 
+    class Calculate
+    {
+        // Конструктор. В параметрі передається посилання на проект
+        Calculate(Project proj)
+        {
+            this.Proj = proj;
+        }
+
+        // Проект, для якого проводиться розрахунок
+        Project Proj;
+    }
+}
