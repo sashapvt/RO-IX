a87748df1e39cd1a03ce1c6ef18ab179900b6879
Sasha <sashapvt@gmail.com>
2016-01-29 23:35:26 +0200

    Розроблений власний парсер шаблонів на базі спрощенного синтаксису Razor.

---
+++ Shared/LiteRazorReport.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class LiteRazorReport
+    {
+        public LiteRazorReport(String _template)
+        {
+            Template = _template;
+            Vars = new Dictionary<String, String>();
+        }
+
+        public String Template;
+        public Dictionary<String, String> Vars;
+
+        public string Report()
+        {
+            report = new StringBuilder(Template.Length);
+            Boolean varDetected = false;
+            Boolean lastAt = false;
+            String varName = "";
+
+            foreach (Char ch in Template)
+            {
+                if (!Char.IsLetterOrDigit(ch))
+                {
+                    varDetected = false;
+                    if (varName != "")
+                    {
+                        if (Vars.ContainsKey(varName)) report.Append(Vars[varName]);
+                        varName = "";
+                    }
+                }
+                if (ch.CompareTo('@') == 0)
+                {
+                    if (lastAt)
+                    {
+                        report.Append(ch);
+                        lastAt = false;
+                    }
+                    else
+                    {
+                        varDetected = true;
+                        lastAt = true;
+                    }
+                }
+                else
+                {
+                    lastAt = false;
+                    if (!varDetected) report.Append(ch); else varName += ch;
+                }
+            }
+            if (varName != "")
+            {
+                if (Vars.ContainsKey(varName)) report.Append(Vars[varName]);
+                varName = "";
+            }
+            return report.ToString();
+        }
+
+        private StringBuilder report;
+    }
+}
---
+++ UnitTestRO-IX/UnitTestLiteRazorReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared;
+
+namespace UnitTestRO_IX
+{
+    [TestClass]
+    public class UnitTestLiteRazorReport
+    {
+        [TestMethod]
+        public void TestMethodLiteRazorReport()
+        {
+            // 1
+            var LRR = new LiteRazorReport("Hello, @w!");
+            LRR.Vars.Add("w", "World");
+            Console.WriteLine(LRR.Template);
+            Console.WriteLine(LRR.Report());
+            Assert.AreEqual("Hello, World!", LRR.Report());
+            // 2
+            LRR = new LiteRazorReport("<vars>@a then @b</vars>");
+            LRR.Vars.Add("a", "one");
+            LRR.Vars.Add("b", "two");
+            Console.WriteLine(LRR.Template);
+            Console.WriteLine(LRR.Report());
+            Assert.AreEqual("<vars>one then two</vars>", LRR.Report());
+            // 3
+            LRR = new LiteRazorReport("A@a, lets show AT: @@. Let's go on one more: @@@a");
+            LRR.Vars.Add("a", "1");
+            Console.WriteLine(LRR.Template);
+            Console.WriteLine(LRR.Report());
+            Assert.AreEqual("A1, lets show AT: @. Let's go on one more: @1", LRR.Report());
+        }
+    }
+}
