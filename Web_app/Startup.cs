729869bbccb283895482fa1fe09421997213a54d
Sasha <sashapvt@gmail.com>
2016-02-02 23:08:48 +0200

    Додав ASP.NET веб проект Web_app

---
+++ Web_app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_app.Startup))]
+namespace Web_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
