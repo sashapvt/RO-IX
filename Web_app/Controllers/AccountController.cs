01bde593915d14ec4c54af00e5a867eb65b5d648
Sasha <sashapvt@gmail.com>
2016-04-24 23:26:58 +0300

    Виправлений перехід після реєстрації на сторінку проектів. Видалено контролер і сторінки Home. Проведено деплой на http://ro-ix.azurewebsites.net/

--- Web_app/Controllers/AccountController.cs
+++ Web_app/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace Web_app.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Project");
                 }
                 AddErrors(result);
             }
@@ -392,7 +392,7 @@ namespace Web_app.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Project");
         }
 
         //
@@ -449,7 +449,7 @@ namespace Web_app.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Project");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
